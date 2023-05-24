7932ab5afd06cf1a892785fe50c9a68c6269b1a6
Alex Kibe <alex.kibe01@student.moringaschool.com>
2023-05-24 21:47:19 +0300

    Completed assignment

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,9 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()
+
+    def match(self, word_list):
+        return [candidate for candidate in word_list if self._is_anagram(candidate)]
+
+    def _is_anagram(self, candidate):
+        return sorted(candidate.lower()) == sorted(self.word) and candidate.lower() != self.word
